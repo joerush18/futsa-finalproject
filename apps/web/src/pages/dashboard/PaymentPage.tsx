2f27b332993a11f157cfc58a4528b893de91e26f
Joey Rush <55552872+joerush18@users.noreply.github.com>
2023-09-04 20:47:25 +0545

    Add docs (#50)
    * Enhance notification feature
    
    * Enhance small ui issue
    
    * Fix auth issue
    
    * Update on issues fix

--- apps/web/src/pages/dashboard/PaymentPage.tsx
+++ apps/web/src/pages/dashboard/PaymentPage.tsx
@@ -1,6 +1,7 @@
 import useCurrentUser from "@/hooks/useCurrentUser";
-import { Box } from "@mui/material";
-import { useGetTransactionByFutsal } from "core";
+import Color from "@/utils/color";
+import { Box, Typography } from "@mui/material";
+import { timeAgo, useGetTransactionByFutsal } from "core";
 
 const PaymentPage = () => {
   const { futsal } = useCurrentUser();
@@ -15,7 +16,48 @@ const PaymentPage = () => {
     <Box>
       {transactions?.length ? (
         transactions.map((d) => {
-          return <Box>{d.amount}</Box>;
+          return (
+            <Box
+              sx={{
+                background: Color.white.main,
+                padding: 2,
+                border: `1px solid ${Color.grey[300]}`,
+                width: "max-content",
+                borderRadius: 2,
+                cursor: "pointer",
+                "&:hover": {
+                  background: Color.grey[200],
+                },
+              }}
+            >
+              <Typography
+                sx={{
+                  fontWeight: 600,
+                }}
+              >
+                You received payment of Rs. {d.amount} from {d.payedBy.name}.
+              </Typography>
+              <Typography
+                variant="caption"
+                sx={{
+                  color: Color.secondary.focus,
+                }}
+              >
+                Txnid : {d.tnxId}
+              </Typography>
+              <br />
+              <Typography
+                variant="caption"
+                sx={{
+                  mt: 1,
+                  color: Color.primary.main,
+                  textTransform: "capitalize",
+                }}
+              >
+                {d.payedfor.collection} id : {d.payedfor.id}
+              </Typography>
+            </Box>
+          );
         })
       ) : (
         <Box>No transactions found.</Box>
